b49bcaa17541cea92d0aad3df25cebd2b2ddf4a7
Hamza Ali <hamzamughal2145@gmail.com>
2022-09-18 11:46:55 -0400

    Coupling

---
+++ lab-00-coupling/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,25 @@
+package com.cydeo;
+
+import com.cydeo.loosely.BalanceManager;
+import com.cydeo.tightly.BalanceService;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Main {
+    public static void main(String[] args) {
+        UUID user = UUID.randomUUID();
+
+        CustomerBalance customerBalance = new CustomerBalance(user, BigDecimal.ZERO);
+        GiftCardBalance giftCardBalance = new GiftCardBalance(user, BigDecimal.ZERO);
+
+        customerBalance.addBalance(new BigDecimal(150));
+        giftCardBalance.addBalance(new BigDecimal(120));
+
+        BalanceManager balanceManager = new BalanceManager();
+
+        System.out.println(balanceManager.checkout(customerBalance, new BigDecimal(80)));
+        System.out.println(balanceManager.checkout(giftCardBalance, new BigDecimal(80)));
+    }
+
+}
