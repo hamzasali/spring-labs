aa54bcb8eeccacfb583688a9d7dd3eb5caeebd98
Hamza Ali <hamzamughal2145@gmail.com>
2022-09-18 12:25:28 -0400

    DI

---
+++ lab-02-di/src/main/java/com/cydeo/model/Product.java
@@ -0,0 +1,16 @@
+package com.cydeo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+@Getter
+@Setter
+@Component
+public class Product {
+    private String name;
+    private int quantity;
+    private int remainingQuantity;
+    private BigDecimal price;
+}
