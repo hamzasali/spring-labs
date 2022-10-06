acd3dd3713f4d97444bd9ecb127ef7c55d1a854b
Hamza Ali <hamzamughal2145@gmail.com>
2022-10-06 12:30:56 -0400

    Spring ThymeLeaf

---
+++ spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/CartController.java
@@ -0,0 +1,15 @@
+package com.cydeo.spring05thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CartController {
+
+    @GetMapping("/cart")
+    public String create(){
+
+        return "/cart/show-cart";
+    }
+}
