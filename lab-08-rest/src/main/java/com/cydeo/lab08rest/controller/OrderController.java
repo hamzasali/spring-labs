dbf9945d8759728fc7062b58529de12c4265e5a1
Hamza Ali <hamzamughal2145@gmail.com>
2022-12-07 18:22:06 -0500

    Spring Rest Order

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/OrderController.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/OrderController.java
@@ -1,4 +1,64 @@
 package com.cydeo.lab08rest.controller;
 
+import com.cydeo.lab08rest.dto.OrderDTO;
+import com.cydeo.lab08rest.enums.PaymentMethod;
+import com.cydeo.lab08rest.model.ResponseWrapper;
+import com.cydeo.lab08rest.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/order")
 public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createOrder(@RequestBody OrderDTO order) {
+        return ResponseEntity
+                .ok(new ResponseWrapper(
+                        "Order Created"
+                        , orderService.create(order)
+                        , HttpStatus.OK));
+    }
+
+    @PutMapping
+    public ResponseEntity<ResponseWrapper> updateOrder(@RequestBody OrderDTO order) {
+        orderService.update(order);
+        return ResponseEntity
+                .ok(new ResponseWrapper(
+                        "Order Updated successfully"
+                        , HttpStatus.OK));
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getOrderList() {
+        return ResponseEntity
+                .ok(new ResponseWrapper(
+                        "Orders Retrieved Successfully"
+                        , orderService.getAll()
+                        , HttpStatus.OK));
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<ResponseWrapper> getOrderListByEmail(@PathVariable String email) {
+        return ResponseEntity
+                .ok(new ResponseWrapper(
+                        "Order retrieved by Email"
+                        , orderService.getAllByEmail(email)
+                        , HttpStatus.OK));
+    }
+
+    @GetMapping("/paymentMethod/{paymentMethod}")
+    public ResponseEntity<ResponseWrapper> getOrderListByPaymentMethod(@PathVariable PaymentMethod paymentMethod) {
+        return ResponseEntity
+                .ok(new ResponseWrapper(
+                        "Order retrieved by Payment Method"
+                        , orderService.getAllByPaymentMethod(paymentMethod)
+                        , HttpStatus.OK));
+    }
 }
